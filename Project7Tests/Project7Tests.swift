1c583bf2665337c0c522657d4132367b2b37d30a
Maksim Li <maximlee.ml@gmail.com>
2024-10-29 21:09:29 +0200

    Initial Commit

---
+++ Project7Tests/Project7Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Project7Tests.swift
+//  Project7Tests
+//
+//  Created by Maksim Li on 29/10/2024.
+//
+
+import Testing
+@testable import Project7
+
+struct Project7Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
