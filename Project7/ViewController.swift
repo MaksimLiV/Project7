1fa5c38b96af123bc7394c25012e7cda58d2d5cc
Maksim Li <maximlee.ml@gmail.com>
2024-11-09 00:10:52 +0200

    @objc func fetchJSON() creation

--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -9,13 +9,11 @@ import UIKit
 
 class ViewController: UITableViewController {
     var petitions = [Petition]()
-    
     var filteredPetitions = [Petition]()
     var isSearching = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         let creditsButton = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))
         navigationItem.rightBarButtonItem = creditsButton
@@ -24,10 +22,13 @@ class ViewController: UITableViewController {
         searchBar.delegate = self
         searchBar.placeholder = "Search petitions"
         searchBar.sizeToFit()
-        
         tableView.tableHeaderView = searchBar
         
-        var urlString: String
+        performSelector(inBackground: #selector(fetchJSON), with: nil)
+    }
+    
+    @objc func fetchJSON() {
+        let urlString: String
         
         if navigationController?.tabBarItem.tag == 0 {
             urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
@@ -35,25 +36,19 @@ class ViewController: UITableViewController {
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
         
-        DispatchQueue.global(qos: .userInitiated).async {
-            [weak self] in
-            if let url = URL(string: urlString) {
-                if let data = try? Data(contentsOf: url) {
-                    self?.parse(json: data)
-                    return
-                }
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+                return
             }
-            self?.showError()
         }
+        performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
     }
     
-    func showError() {
-        DispatchQueue.main.async { [weak self] in
-            let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default))
-            self?.present(ac, animated: true)
-        }
-        
+    @objc func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed, please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
     func parse(json: Data) {
@@ -61,11 +56,11 @@ class ViewController: UITableViewController {
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
-            
-            DispatchQueue.main.async { [weak self] in
-                self?.tableView.reloadData()
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
             }
-            
+        } else {
+            performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
         }
     }
     
@@ -88,7 +83,7 @@ class ViewController: UITableViewController {
     }
     
     @objc func showCredits() {
-        let ac = UIAlertController (title: "Data Source", message: "The data comes from Hacking with Swift's sample petitions.", preferredStyle: .alert)
+        let ac = UIAlertController(title: "Data Source", message: "The data comes from Hacking with Swift's sample petitions.", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
     }
